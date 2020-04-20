fb2666468dbb9132aaeeb44282ea9a6e5be11342
clintsearl <clintsearl@gmail.com>
2020-04-20 15:54:40 -0600

    First commit, already have it fetching the quote.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+
 import './App.css';
+// import {usefetchAff} from './fetchComponent';
 
 function App() {
-  return (
+
+  // usefetchAff(Url)
+  const Url = `https://quote-garden.herokuapp.com/api/v2/quotes/random`
+  const [affirmation, setAff] = useState([])
+  
+  useEffect(()=> {
+      fetch(Url)
+         .then(response => response.json())
+         .then( data => setAff(data.quote))
+},[Url])
+
+
+console.log(affirmation.quoteText)
+
+return (
+    
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      
+        <h1>
+         Rate the Quote!
+        </h1>
+        <div>{affirmation.quoteText}</div>
+        <div>{affirmation.quoteAuthor}</div>
       </header>
     </div>
   );
---
+++ src/fetchComponent.js
@@ -0,0 +1,14 @@
+import useEffect from 'react'
+// import Url from './App.js'
+  
+// const [affirmation, setAff] = useState([])
+
+// export function usefetchAff(Url){
+//   useEffect(()=> {
+//       fetch(Url)
+//          .then(response => response.json())
+//          .then( data => console.log(data))
+// },[Url])
+
+// }
+// export default usefetchAff()
